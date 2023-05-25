bcb0af41ab6e6a55bd4387130a79977b1ef18f19
kingnoel <66368728+kingnoel@users.noreply.github.com>
2023-05-25 22:47:39 +0100

    first done with about page

---
+++ assets/js/date.js
@@ -0,0 +1,5 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Update the element with the current year
+document.getElementById("currentYear").innerHTML = currentYear;
---
+++ includes/pagefooter.php
@@ -0,0 +1,104 @@
+<div class="footer-inner">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="widget-footer">
+                    <div class="widget widget-logo">
+                        <div class="logo-bottom" id="logo-footer">
+                            <a href="index.html"><img src="assets/images/logo/logofootert.png" alt="kinco"></a>
+                        </div>
+                        <p class="wrap f-mulish">Experience the power of our offerings and unlock new possibilities in education.</p>
+                        <div class="list-contact">
+                            <ul>
+                                <li class="fx"><span><i class="far fa-map-marker-alt"></i> 3 Plerooville Crescent Karshi</span></li>
+                                <li class="fx"><a href="mailto:hotline@gmail.com"><i class="far fa-envelope"></i>info@plerooville.com.ng</a></li>
+                                <li class="fx"><a href="tel:+2348032761556"><i class="fal fa-phone"></i> +234(0) 803 276 1556</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="widget widget-business">
+                        <div class="inner">
+                            <div class="op-time">
+                                <h4 class="title-widget">opening hours</h4>
+                                <ul>
+                                    <li><span class="f-mulish">Monday - Friday</span></li>
+                                    <li><span class="f-mulish">12:00am - 05:00pm</span></li>
+                                    &nbsp;
+                                    <div class="cls-time">
+                                        <p>Govt Holiday &</p>
+                                    </div>
+                                    <li><span class="f-mulish">Saturday & Sunday</span></li>
+                                    <li><span class="f-mulish">10:00am - 03:00pm</span></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="widget widget-link">
+                        <h4 class="title-widget">Our Program</h4>
+                        <ul class="list-link">
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">Coding & Tech</a></li>
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">English </a></li>
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">Languages</a></li>
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">Math</a></li>
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">Arts</a></li>
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">Science</a></li>
+                            <li class="fx"><a href="program.html" class="wd-ctm f-mulish">Life Skills & Games</a></li>
+                        </ul>
+                    </div>
+                    <div class="widget widget-news st-4">
+                        <h4 class="title-widget">recent news</h4>
+                        <ul class="list-news">
+                            <li class="fx">
+                                <img src="assets/images/thumbnails/widget9.jpg" alt="Image" class="feature">
+                                <ul class="box-content">
+                                    <li>
+                                        <h6 class="title"><a href="#">Do you know that Greenery boosts Learning and Retention?</a></h6>
+                                    </li>
+                                    <li><a href="#" class="fx meta-news clr-pri-6"><i class="far fa-calendar-alt"></i>07 Mar 2023</a></li>
+                                </ul>
+                            </li>
+                            <li class="fx">
+                                <img src="assets/images/thumbnails/widget10.jpg" alt="Image" class="feature">
+                                <ul class="box-content">
+                                    <li>
+                                        <h6 class="title"><a href="#">Running an Afterschool program</a></h6>
+                                    </li>
+                                    <li><a href="#" class="fx meta-news clr-pri-6"><i class="far fa-calendar-alt"></i>25 May 2023</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="footer-bottom jus-ct">
+                    <p class="copy-right">&copy; <span id="currentYear"></span> Plerooville. All rights reserved. Designed by <a href="https://digitalplanetx.tech">Digital Planet X</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+</div>
+</div>
+<!-- /#wrapper -->
+
+<a id="scroll-top"></a>
+
+<!-- Javascript -->
+<script src="assets/js/date.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/plugin.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/owl.carousel2.thumbs.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/shortcodes.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/jquery-validate.js"></script>
+</body>
+
+
+<!-- Mirrored from themesflat.co/kinco/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 27 Apr 2023 04:20:16 GMT -->
+
+</html>
